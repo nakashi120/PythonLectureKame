31625d7f375c87ebbe2b0ff2f8680185c042f472
nakashi120 <nakashi120@gmail.com>
2021-07-12 23:55:38 +0900

    None handling in if statement

--- basic/if_non.py
+++ basic/if_non.py
@@ -1,3 +1,10 @@
 # if文でのNoneの取り扱い
-a = None
-a is None
+a = None  # 「なにも値が入っていない」が入っている
+
+# if a is None:
+#     print("a is None!")
+# else:
+#     print("a has value!")
+
+if not a:
+    print("a is None")
