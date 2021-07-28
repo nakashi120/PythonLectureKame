ac7aa9d25c481b8c16d93a04a3c50c15258c09a9
nakashi120 <nakashi120@gmail.com>
2021-07-28 15:43:00 +0900

    decorator

---
+++ function/decorator.py
@@ -0,0 +1,23 @@
+# Decorator: 関数に機能を付属する（デコレートする）
+
+
+def greeting(func):
+    def inner(*args, **kwargs):
+        print("Hello!")
+        func(*args, **kwargs)
+        print("Nice to meet you!")
+    return inner
+
+
+@greeting
+def say_name(name):
+    print(f"I'm {name}")
+
+
+@greeting
+def say_name_and_origin(name, origin):
+    print(f"I'm {name}, I'm from {origin}")
+
+
+# say_name("Jiro")
+say_name_and_origin("Nakashi", 'Osaka')
