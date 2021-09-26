32e6b0a01223a1176567e8a790d53d78335e7183
nakashi120 <nakashi120@gmail.com>
2021-09-26 23:33:44 +0900

    python test

---
+++ pythontest/tests/test_power_pytest.py
@@ -0,0 +1,17 @@
+import pytest
+
+from power import power, times
+
+
+def test_power():
+    base = 2
+    exp = 3
+    assert power(base, exp) == 8
+    with pytest.raises(TypeError):
+        power("3", "2")
+
+
+def test_times():
+    num1 = 2
+    num2 = 3
+    assert times(num1, num2) == 6
